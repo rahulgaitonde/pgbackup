3d031bd4dc4b7ac306cc56e7e3bf230ae7fd0357
rahulgaitonde1989 <gaitonderahul@gmail.com>
2018-12-22 17:49:44 -0500

    mek file

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -20,3 +20,15 @@ def test_parser_with_driver():
     parser = cli.create_parser()
     with pytest.raises(SystemExit):
         parser.parse_args([url, "--driver", "local"])
+
+def test_parser_with_driver_and_destination():
+    """
+    The parser will not exit if it receives a driver
+    with a destination
+    """
+    parser = cli.create_parser()
+
+    args = parser.parse_args([url, "--driver", "local", "/some/path"])
+
+    assert args.driver == "local"
+    assert args.destination == "/some/path"
