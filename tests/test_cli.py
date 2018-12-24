ddc3dcbd3008feab9b5ece4006a8c52ee92550d3
rahulgaitonde1989 <gaitonderahul@gmail.com>
2018-12-24 10:11:17 -0500

    created a new cli.py file

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -21,14 +21,36 @@ def test_parser_with_driver():
     with pytest.raises(SystemExit):
         parser.parse_args([url, "--driver", "local"])
 
+
+
+def test_parser_with_unknown_driver():
+    """
+    The parser will exit if the driver name is unknown
+    """
+parser = cli.create_parser()
+with pytest.raises(SystemExit):
+    parser.parse_args([url, '--driver', 'azure', 'destination'])
+
+
+
+def test_parser_with_know_driver():
+    """
+    The parser will not exit if the driver is know
+    """
+
+    parser = cli.create_parser()
+
+    for driver in ['local', 's3']:
+        assert parser.parse_args([url, 'driver', driver, 'destination'])
+
+
+
 def test_parser_with_driver_and_destination():
     """
     The parser will not exit if it receives a driver
     with a destination
     """
     parser = cli.create_parser()
-
     args = parser.parse_args([url, "--driver", "local", "/some/path"])
-
     assert args.driver == "local"
     assert args.destination == "/some/path"
