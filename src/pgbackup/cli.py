07d6d172519847398f8c881ffb04b7b0cd1e6f3d
rahulgaitonde1989 <gaitonderahul@gmail.com>
2018-12-27 15:56:00 -0500

    updated the cli.py and setup.py

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,3 +1,4 @@
+import argparse
 from argparse import Action, ArgumentParser
 
 
@@ -13,15 +14,15 @@ class DriverAction(Action):
         namespace.destination = destination
 
 def create_parser():
-    parser = ArgumentParser(description="""
+    parser = argparse.ArgumentParser(description="""
     Back up PostgreSQL databases locally or to AWS S3.
     """)
-    parser.add_argument("url", help="URL of the PostgreSQL database to backup")
-    parser.add_argument("--driver", 'd',
+    parser.add_argument("url", help="URL of database to backup")
+    parser.add_argument("--driver", "-d",
             help="how & where to store backup",
             nargs=2,
+            metavar=("DRIVER", "DESTINATION"),
             action=DriverAction,
-            metavar=('driver', 'destination'),
             required=True)
     return parser
 
