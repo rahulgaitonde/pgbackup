ceb63cff1f57f0b1634b70f177353460372256f3
rahulgaitonde1989 <gaitonderahul@gmail.com>
2018-12-26 19:10:02 -0500

    updated the cli.py and setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import setup, find_packages
 with open('README.rst', encoding='UTF-8') as f:
     readme = f.read()
 
+
+    install_requires = ['boto3'],
+    entry_points = {
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+        ],
+    }
     setup(
         name='pgbackup',
         version='0.1.0',
